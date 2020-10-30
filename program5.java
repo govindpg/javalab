a6872b2075d7c88f4df7c8c5e9ff84df96c718d4
Govind P G <72184197+govindpg@users.noreply.github.com>
2020-10-30 11:02:08 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Program5{
+    public static void main(String[] args){
+        int n=100,i,sum=0;
+        for(i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum = "+sum);
+    }
+}
