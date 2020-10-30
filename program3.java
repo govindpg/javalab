bf2f2745f930a141a072ccd6af8f82dfcb1f79c5
Govind P G <72184197+govindpg@users.noreply.github.com>
2020-10-30 10:01:35 +0530

    Create program3.java

---
+++ program3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Program3 {
+    public static void main(String[] args) {
+        int a=1000;
+        if (a%2==0)
+         System.out.println("a is an even number");
+        else
+         System.out.println("a is odd number");
+
+    }
+}
