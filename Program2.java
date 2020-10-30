896f4cc93bc3aeb7675bbc0b9c93f387c1b27c6e
Govind P G <72184197+govindpg@users.noreply.github.com>
2020-10-30 13:54:43 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -3,6 +3,9 @@ public class Program2 {
     public static void main(String[] args) {
         int a=6,b=4;
         int c=a+b;
-        System.out.println(c);
+        System.out.println("the sum is:"+c);
     }
     }
+
+OUTPUT
+the sum is:10
